1efb885131a56c7117044748ee1a770c17904643
Kevin Smith <kesmith@indeed.com>
2020-04-30 18:10:53 -0700

    Use default input/output paths; improve comments

--- src/indexExample.ts
+++ src/indexExample.ts
@@ -1,18 +1,24 @@
-import * as mariner from './mariner/index'; // in your code this should be `import * as mariner from 'oss-mariner'`
+/*  This is an example of how to invoke mariner
+    It mostly exists to be able to test and debug while working on the library code
 
-/* This is an example of how to invoke DependencyDetailsRetriever
-   It mostly exists to be able to test and debug while working on the library code
+    To use it, you will need to add a variable to your environment:
+        export GITHUB_TOKEN="<token here>"
+    Optionally, you can have environment variables for INPUT_FILE_PATH and OUTPUT_FILE_PATH,
+        but they have defaults that will work with the standard development environment.
+    Then, run `npm run build`
+    Finally, run `node dist/indexExample.js`
 
-   To use it, you will need to add 3 variables to your environment:
-       export GITHUB_TOKEN="<token here>"
-       export INPUT_FILE_PATH="/Users/{yourUserHere}/{path}/{to}/exampleData/mini.json"
-       export OUTPUT_FILE_PATH="/Users/{yourUserHere}/{path}/{to}/exampleData/analysisOutputRaw.json"
-  Then, run `npm run build`
-  Finally, run `node dist/indexExample.js`
-
-  FYI, if it's running correctly, your first logged output in the console will likely say "ERROR". Sorry about that ;-)
-       You'll know it's run correctly if you have a new file exampleData/analysisOutputRaw.json with some GitHub issues in it.
+    FYI, if it's running correctly, your first logged output in the console
+    will likely say "ERROR". Sorry about that ;-)
+        You'll know it's run correctly if you have a new file
+        exampleData/output.json with some GitHub issues in it.
 */
+
+import * as mariner from './mariner/index'; // This is used during development
+// import * as mariner from 'oss-mariner'    // This is how it the npm package would normally be used
+
+import * as path from 'path';
+
 function getFromEnvOrThrow(configField: string): string {
     const value = process.env[configField];
     if (!value) {
@@ -21,10 +27,17 @@ function getFromEnvOrThrow(configField: string): string {
 
     return value as string;
 }
-const token = getFromEnvOrThrow('GITHUB_TOKEN');
-const inputFilePath = getFromEnvOrThrow('INPUT_FILE_PATH');
-const outputFilePath = getFromEnvOrThrow('OUTPUT_FILE_PATH');
 
+const token = getFromEnvOrThrow('MARINER_GITHUB_TOKEN');
+const inputFilePath =
+    process.env.INPUT_FILE_PATH || path.join(__dirname, '..', 'exampleData/mini.json');
+const outputFilePath =
+    process.env.OUTPUT_FILE_PATH || path.join(__dirname, '..', 'exampleData/output.json');
+
+/*  This demonstrates instructing mariner to use a custom logger.
+    It is optional, and if you don't call setLogger,
+    output will simply be sent to console.log()
+    */
 class FancyLogger implements mariner.Logger {
     public info(message: string): void {
         console.log('***INFO: ' + message);
@@ -34,8 +47,12 @@ class FancyLogger implements mariner.Logger {
     }
 }
 
-// Use a custom logger instead of the default console logger (this is optional!)
-mariner.setLogger(new FancyLogger());
+const logger = new FancyLogger();
+mariner.setLogger(logger);
 
+logger.info(`Input:  ${inputFilePath}`);
+logger.info(`Output: ${outputFilePath}`);
+
+// Here is the code to actually call mariner
 const ddr = new mariner.DependencyDetailsRetriever();
 ddr.run(token, inputFilePath, outputFilePath);
