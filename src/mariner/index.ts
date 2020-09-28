4dce30ef7aa8dde16e095e40722effe8dfff7d11
ingrid <igvillela@indeed.com>
2020-09-28 16:03:09 -0700

    deleted the export of GitHubFetcher and GitHubIssue

--- src/mariner/index.ts
+++ src/mariner/index.ts
@@ -1,4 +1,3 @@
 export { DependencyDetailsRetriever } from '../dependency-details-retriever';
 export { Issue, IssueFinder } from '../issueFinder';
-export { GitHubIssueFetcher, GitHubIssue } from '../gitHubIssueFetcher';
 export { Logger, setLogger } from '../tab-level-logger';
