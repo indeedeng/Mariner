993b37dee660493e80c7feb5fd004b2dac05e548
ingrid <aleja.morada@gmail.com>
2021-02-18 11:05:00 -0800

    Added luxon Datetime const to deal with maxIssueAge, cleaned up two passing tests
    Co-authored-by: Kevin Smith <45774061+kevindigo@users.noreply.github.com>

--- src/__tests__/generateConfluenceMarkdown.test.ts
+++ src/__tests__/generateConfluenceMarkdown.test.ts
@@ -1,10 +1,12 @@
 import { generateConfluenceMarkdown } from '../Utilities/generateConfluenceMarkdown';
 import { Issue } from '../issueFinder';
+import { DateTime, Duration } from 'luxon';
 
+const eightDaysAgo = DateTime.utc().minus(Duration.fromISO('P8D')).toISO();
 const fakeIssues: Issue[] = [
     {
         title: 'Formatting seems to use Prettier options by default',
-        createdAt: '2020-11-30T16:51:33Z',
+        createdAt: eightDaysAgo,
         repositoryNameWithOwner: 'bc/typescript',
         url: 'https://github.com/bc/typescript/issues/30',
         updatedAt: '',
@@ -13,7 +15,7 @@ const fakeIssues: Issue[] = [
 
     {
         title: 'style config',
-        createdAt: '2014-11-04T23:10:02Z',
+        createdAt: eightDaysAgo,
         repositoryNameWithOwner: 'material-ui/mui',
         url: 'https://github.com/material-ui/issues/24',
         updatedAt: '',
@@ -24,24 +26,23 @@ const fakeIssues: Issue[] = [
 describe('generateConfluenceMarkdown function', () => {
     it('should pass in a dependency with no issues', () => {
         const mapWithoutIssues: Map<string, Issue[]> = new Map();
-        // const noIssues: Issue[] = [];
+        const noIssues: Issue[] = [];
         const dependency = 'TestDependency';
-        const oneDependency = mapWithoutIssues.set(dependency, fakeIssues);
-
+        const oneDependency = mapWithoutIssues.set(dependency, noIssues);
         const results = generateConfluenceMarkdown(oneDependency, 30);
-        expect(results).toContain([]); // expecting it to fail because fakeIssues has two issues...
+
+        expect(results).toContain(noIssues);
     });
 
-    // Tests WIP
-    // it('should return 2 issues for a single dependency', () => {
-    //     const mockDependencyMap: Map<string, Issue[]> = new Map();
-    //     const dependency2 = 'NodeJsDependency';
-    //     const twoIssues = mockDependencyMap.set(dependency2, fakeIssues);
-    //     const dep = mockDependencyMap.get(dependency2[1]);
+    it('should return 2 issues for a single dependency', () => {
+        const mockDependencyMap: Map<string, Issue[]> = new Map();
+        const dependency = 'NodeJsDependency';
+        const twoIssues = mockDependencyMap.set(dependency, fakeIssues);
 
-    //     const results = generateConfluenceMarkdown(twoIssues);
-    //     expect(results).toContain(dep);
-    // });
+        const results = generateConfluenceMarkdown(twoIssues);
+        expect(results).toContain(fakeIssues[0].title);
+        expect(results).toContain(fakeIssues[0].title);
+    });
     //      it('should return two dependencies, each with their issue', () => {
     //         const depsMap: Map<string, Issue[]> = new Map();
     //         const a = generateConfluenceMarkdown(dependency);
@@ -51,6 +52,11 @@ describe('generateConfluenceMarkdown function', () => {
     //          // expect().toContain();
     //      });
     //       it('should return an issue that has square brackets and curly braces in its title', () => {
+    //        // failing test
+
+    //           expect(generateConfluenceMarkdown(a, b))
+    //     });
+    //       it('should pass in a dependency with one issue and make sure the all fields for that issue are correct', () => {
     //        // failing test
 
     //           expect(generateConfluenceMarkdown(a, b))
