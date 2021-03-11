2cd5a16a5ee96b05396e50a0668c6c819cd66665
ingrid <aleja.morada@gmail.com>
2021-03-11 13:52:07 -0800

    edited code based on review and changed tests & exported function calculate to use in test

--- src/Utilities/generateConfluenceMarkdown.ts
+++ src/Utilities/generateConfluenceMarkdown.ts
@@ -5,7 +5,7 @@ export function generateConfluenceMarkdown(
     issuesByDependency: Map<string, Issue[]>,
     maxIssuesAge = 30
 ): string {
-    const now = DateTime.local();
+    const now = DateTime.utc();
 
     const markdownArray: string[] = [];
 
@@ -15,24 +15,40 @@ export function generateConfluenceMarkdown(
         if (!issues || !issues.length) {
             continue;
         }
-        issues.forEach((issue) => {
-            const createdAt = DateTime.fromISO(issue.createdAt);
-            const ageInDays = now.diff(createdAt, 'days').days;
-            const ageInWholeDays = Math.round(ageInDays);
 
-            if (ageInWholeDays < maxIssuesAge) {
-                markdownArray.push('\n');
-                markdownArray.push(`h3. ${dependency}`);
-                markdownArray.push('||*Title*||*Age*||');
-                const cleanedTitleMarkdown = cleanMarkdown(issue.title);
-                markdownArray.push(
-                    `|[${cleanedTitleMarkdown}|${issue.url} ]|${ageInWholeDays}&nbsp;days|`
-                );
-            }
+        const relevantIssues = issues.filter((issue) => {
+            const ageInWholeDays = calculateAgeInWholeDays(issue.createdAt, now);
+
+            return ageInWholeDays < maxIssuesAge;
+        });
+
+        if (!relevantIssues.length) {
+            continue;
+        }
+
+        markdownArray.push('\n');
+        markdownArray.push(`h3. ${dependency}`);
+        markdownArray.push('||*Title*||*Age*||');
+
+        relevantIssues.forEach((issue) => {
+            const ageInWholeDays = calculateAgeInWholeDays(issue.createdAt, now);
+
+            const cleanedTitleMarkdown = cleanMarkdown(issue.title);
+            markdownArray.push(
+                `|[${cleanedTitleMarkdown}|${issue.url}]|${ageInWholeDays}&nbsp;days|`
+            );
         });
     }
 
     return markdownArray.join('\n');
+}
+
+export function calculateAgeInWholeDays(isoDateString: string, now: DateTime): number {
+    const createdAt = DateTime.fromISO(isoDateString);
+    const ageInDays = now.diff(createdAt, 'days').days;
+    const ageInWholeDays = Math.round(ageInDays);
+
+    return ageInWholeDays;
 }
 
 export function cleanMarkdown(issueTitle: string): string {
