d6d3fabef02a56b7bb32089cd10a1d2cd71b3b72
ingrid <aleja.morada@gmail.com>
2021-03-22 17:16:00 -0700

    updated logger.info() message and edited code adding JSON.stringify

--- src/gitHubIssueFetcher.ts
+++ src/gitHubIssueFetcher.ts
@@ -197,7 +197,7 @@ export class GitHubIssueFetcher {
         }
         result.edges = result.edges.filter((edge) => {
             if (!edge.node.repository) {
-                getLogger().info(`\nNo repository for ${Object.values(variables)}`);
+                getLogger().info(`\nNo repository for an edge in ${JSON.stringify(variables)}`);
             }
 
             return edge.node.repository;
