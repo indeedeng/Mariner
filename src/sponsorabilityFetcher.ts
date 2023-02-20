6b26eeaec0b085db9d53ab45570b8bcb34fbdcc1
IngridGdesigns <garciai@protonmail.com>
2023-02-20 13:21:00 -0800

    updated interfaces, added to fetchSponsorData

--- src/sponsorabilityFetcher.ts
+++ src/sponsorabilityFetcher.ts
@@ -1,13 +1,35 @@
 import { Config } from './config';
 import { Contributor, ContributorFetcher } from './contributorFetcher';
-import { graphql, GraphQlQueryResponseData } from '@octokit/graphql';
+import { graphql } from '@octokit/graphql'; // GraphQlQueryResponseData
 import { RequestParameters } from '@octokit/graphql/dist-types/types';
 
-// query WIP:
+interface User {
+    login: string;
+    name: string;
+    url: string;
+}
+
+interface Organization {
+    login: string;
+    url: string;
+    bio: string;
+}
+interface Sponsorable {
+    user: {
+        sponsors: {
+            totalCount: number; // total  sponsor count
+            nodes: {
+                user: User[];
+                organization: Organization[];
+            };
+        };
+    };
+}
+// query WIP: fetchiing first 10 sponsors, need to add pagination
 const queryTemplate = `query fetchSponsorable($userLogin: String!) {
   user(login: $userLogin) {
     ... on Sponsorable {
-      sponsors(first: 10) {
+      sponsors(first: 10) { 
         totalCount
         nodes {
           ... on User { login, name, url  }
@@ -57,17 +79,16 @@ export class SponsorabilityFetcher {
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
     ): Promise<any> {
         console.log(contributors.length);
-        const testContributorArray = [{ login: 'filiptronicek' }, { login: 'IngridGdesigns' }]; // test data
+        const testContributorArray = [{ login: 'filiptronicek' }, { login: 'mvdan' }]; // test data
         let contributorSponsorInfo;
 
         for (const contributor of testContributorArray) {
             // Add Contributors here
             const userLogin = contributor.login;
             const variables: Variables = { userLogin };
-            console.log(variables);
 
             const response = await this.fetchSponsorData(token, variables, query);
-            console.log(response);
+            console.log(response.user.sponsors.nodes);
             // contributorSponsorInfo.push(...[response]);
         }
 
@@ -79,12 +100,12 @@ export class SponsorabilityFetcher {
         token: string,
         variables: Variables,
         query: string
-    ): Promise<GraphQlQueryResponseData> {
+    ): Promise<Sponsorable> {
         const graphqlWithAuth = graphql.defaults({
             headers: { authorization: `token ${token}` },
         });
 
-        const response: GraphQlQueryResponseData = await graphqlWithAuth(query, variables);
+        const response: Sponsorable = await graphqlWithAuth(query, variables);
 
         return response;
     }
