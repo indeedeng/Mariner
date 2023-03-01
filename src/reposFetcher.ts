2834406571b3c0b3d45dd0b5fccd002710477a8a
IngridGdesigns <garciai@protonmail.com>
2023-03-01 04:09:57 -0800

    rename const, fetchRepos graphql call

--- src/reposFetcher.ts
+++ src/reposFetcher.ts
@@ -1,9 +1,10 @@
 import { Config } from './config';
-import { RequestParameters } from '@octokit/graphql/dist-types/types';
+import { GraphQlQueryResponseData, RequestParameters } from '@octokit/graphql/dist-types/types';
 import { User } from './sponsorabilityFetcher';
+import { graphql } from '@octokit/graphql'; // GraphQlQueryResponseData
 
-const queryTemplate = `query fetchRepos($userLogins: String!) {
-  search(query: $userLogins, type: REPOSITORY, first: 10) {
+const queryTemplate = `query fetchRepoInfo($login: String!) {
+  search(query: $login, type: REPOSITORY, first: 10) {
     nodes {
       ... on Repository {
         name
@@ -20,7 +21,7 @@ const queryTemplate = `query fetchRepos($userLogins: String!) {
 }`;
 
 interface Variables extends RequestParameters {
-    userLogin: string;
+    login: string;
 }
 
 export class ReposFetcher {
@@ -35,8 +36,11 @@ export class ReposFetcher {
         // create a loop through allUsers..
         const repoNameAndLanguages: User[] = [];
         allUsers.forEach(async (user) => {
-            const variables: Variables = { userLogin: user.login };
-            const sponsorable = await this.fetchRepos(token, queryTemplate, variables);
+            console.log(`Line 39: ${user.login}`);
+            const login = user.login;
+
+            const variables: Variables = { login };
+            const sponsorable = await this.fetchRepos(token, variables, queryTemplate);
             repoNameAndLanguages.push(sponsorable); // temporary
         });
 
@@ -45,18 +49,53 @@ export class ReposFetcher {
 
     public async fetchRepos(
         token: string,
-        query: string,
-        variables: Variables
+        variables: Variables,
+        query: string
 
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
     ): Promise<any> {
-        // get repos;
-        console.log(token.length, query.length, variables.userLogin);
+        const graphqlWithAuth = graphql.defaults({
+            headers: { authorization: `token ${token}` },
+        });
+
+        const response: GraphQlQueryResponseData = await graphqlWithAuth(query, variables);
+
+        const result = response.search;
+        console.log(`fetchRepos function line 61: ${JSON.stringify(result, null, 2)}`);
+
+        return result;
     }
     //To-do:
     /*
-    . add repo fetcher functions
     . added types/interfaces
     . fix contributorFetcher.ts file - may need support
     */
 }
+// output
+// {
+//   "nodes": [
+//     {
+//       "name": "laurabeatris.com",
+//       "languages": {
+//         "edges": [
+//           {
+//             "node": {
+//               "name": "TypeScript"
+//             }
+//           },
+//           {
+//             "node": {
+//               "name": "JavaScript"
+//             }
+//           }
+//         ]
+//       }
+//     },
+//     {
+//       "name": "LauraBeatris",
+//       "languages": {
+//         "edges": []
+//       }
+//     }
+//   ]
+// }
