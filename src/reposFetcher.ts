a9b380113291cf7dcd4e3e6e27cc9bd7d4658df2
IngridGdesigns <garciai@protonmail.com>
2023-03-01 01:33:51 -0800

    added reposFetcher class

---
+++ src/reposFetcher.ts
@@ -0,0 +1,16 @@
+import { Config } from './config';
+
+export class ReposFetcher {
+    private readonly config: Config;
+
+    public constructor(config: Config) {
+        this.config = config;
+    }
+    //To-do:
+    /*
+    1. figure out query
+    2. add repo fetcher functions
+    3. added types/interfaces
+    4. fix contributorFetcher.ts file - may need support
+    */
+}
