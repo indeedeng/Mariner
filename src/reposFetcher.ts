a32a879adb21c29f3026d3f3beb8758968912a2e
IngridGdesigns <garciai@protonmail.com>
2023-03-01 01:52:43 -0800

    added fetchContributorRepos function, bullding fetchContributorRepos

--- src/reposFetcher.ts
+++ src/reposFetcher.ts
@@ -1,7 +1,7 @@
 import { Config } from './config';
+import { RequestParameters } from '@octokit/graphql/dist-types/types';
 
-const queryTemplate = `
-query fetchRepos($userLogins: String!) {
+const queryTemplate = `query fetchRepos($userLogins: String!) {
   search(query: $userLogins, type: REPOSITORY, first: 10) {
     nodes {
       ... on Repository {
@@ -18,17 +18,40 @@ query fetchRepos($userLogins: String!) {
   }
 }`;
 
+interface Variables extends RequestParameters {
+    userLogin: string;
+}
+
 export class ReposFetcher {
     private readonly config: Config;
 
     public constructor(config: Config) {
         this.config = config;
     }
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    public async fetchSponsorabilityInformation(token: string, allUsers: string[]): Promise<any> {
+        // create a loop through allUsers..
+
+        const variables: Variables = { userLogin: 'userLogin' };
+        const sponsorable = await this.fetchContributorRepos(token, queryTemplate, variables);
+
+        return sponsorable;
+    }
+
+    public async fetchContributorRepos(
+        token: string,
+        query: string,
+        variables: Variables
+
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    ): Promise<any> {
+        // get repos;
+    }
     //To-do:
     /*
-    1. figure out query
-    2. add repo fetcher functions
-    3. added types/interfaces
-    4. fix contributorFetcher.ts file - may need support
+    . add repo fetcher functions
+    . added types/interfaces
+    . fix contributorFetcher.ts file - may need support
     */
 }
