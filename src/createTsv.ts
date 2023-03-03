83150475b4479fbdffe33036e88070eac24e79fb
IngridGdesigns <garciai@protonmail.com>
2023-03-02 17:02:17 -0800

    added createTsv file

---
+++ src/createTsv.ts
@@ -0,0 +1,5 @@
+//
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function createTsv(data: any): void {
+    console.log(`${data.length} successfully created file here!`);
+}
