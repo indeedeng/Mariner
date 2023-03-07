1764d45217a61bff5ee364726f419a22c919524c
IngridGdesigns <garciai@protonmail.com>
2023-03-07 12:50:34 -0800

    WIP: counting projects, counting language ocurrences

--- src/reposFetcher.ts
+++ src/reposFetcher.ts
@@ -23,7 +23,7 @@ interface Languages {
 
 const queryTemplate = `query fetchRepoInfo($login: String!) {
   search(query: $login, type: REPOSITORY, first: 10) {
-    nodes {
+ nodes {
       ... on Repository {
         name
         languages(first: 10) {
@@ -42,6 +42,17 @@ interface Variables extends RequestParameters {
     login: string;
 }
 
+export interface UserRepo {
+    login: string;
+    projectCount: number;
+    impactCount: number;
+    java: number;
+    javascript: number;
+    python: number;
+    go: number;
+    other: number;
+}
+
 export class ReposFetcher {
     private readonly config: Config;
 
@@ -51,16 +62,43 @@ export class ReposFetcher {
 
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     public async fetchSponsorableRepoInfo(token: string, allUsers: User[]): Promise<any> {
-        const repoNameAndLanguages: Node[][] = [];
-        allUsers.forEach(async (user) => {
-            // console.log(`Line 39: ${user.login[10]}`);
+        const repoNameAndLanguages = [];
+        for (const user of allUsers) {
             const login = user.login;
 
             const variables: Variables = { login };
             const sponsorable = await this.fetchRepos(token, variables, queryTemplate);
-            console.log(sponsorable);
-            repoNameAndLanguages.push(sponsorable); // temporary
-        });
+            // console.log(sponsorable);
+            repoNameAndLanguages.push({ login: user.login, repo: sponsorable }); // temporary
+        }
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        const count: any = {};
+
+        for (const userRepo of repoNameAndLanguages) {
+            const countProjects = userRepo.repo.reduce((counter, obj) => {
+                if (obj.name) {
+                    console.log(obj.name);
+                    counter += 1;
+                }
+
+                return counter;
+            }, 0);
+
+            console.log(countProjects); // project counts
+            const userProjectLanguages = Object.values(userRepo.repo);
+
+            for (const project of userProjectLanguages) {
+                project.languages.edges.forEach((language) => {
+                    // console.log(language.node.name, userRepo.login);
+                    // const name = project.name;
+                    if (count[language.node.name]) {
+                        count[language.node.name] += 1;
+                    } else {
+                        count[language.node.name] = 1;
+                    }
+                });
+            }
+        }
 
         return repoNameAndLanguages;
     }
@@ -76,7 +114,7 @@ export class ReposFetcher {
         )) as GitHubResponse;
 
         const result: Repos = response.search;
-        console.log(`fetchRepos function line 61: ${JSON.stringify(result, null, 2)}`);
+        // console.log(`fetchRepos function line 81: ${JSON.stringify(result, null, 2)}`);
 
         return result.nodes;
     }
@@ -85,4 +123,33 @@ export class ReposFetcher {
     . added types/interfaces - test them
     . add function
     */
+
+    public countLanguages(repo: string[]): string[] {
+        const a = repo.map((type) => {
+            let language = '';
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any
+            const languages: any = {
+                java: function () {
+                    language = 'Java';
+                },
+                javascript: function () {
+                    language = 'Javascript';
+                },
+                python: function () {
+                    language = 'Python';
+                },
+                go: function () {
+                    language = 'Go';
+                },
+                default: function () {
+                    language = 'Other';
+                },
+            };
+            console.log(`the language for this repo is ${language}`);
+
+            return (languages[type] || languages.defualt)();
+        });
+
+        return a;
+    }
 }
