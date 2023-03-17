e69ebeaeb0e4935648285bbb858fa5d159e111f2
IngridGdesigns <garciai@protonmail.com>
2023-03-16 17:34:23 -0700

    implement renames, comment out code, removed contributionCounts from conversion function

--- src/sponsorabilityFinder.ts
+++ src/sponsorabilityFinder.ts
@@ -1,16 +1,17 @@
 import { Config } from './config';
 
-import { SponsorableContributorsFetcher, queryTemplate } from './sponsorableContributorsFetcher';
-import { Languages, RepoLanguagesFetcher } from './reposFetcher';
+import {
+    SponsorableContributorsFetcher,
+    queryTemplate,
+    GithubSponsorable,
+} from './sponsorableContributorsFetcher';
+import { RepoLanguagesFetcher } from './reposFetcher'; // Languages,
 import {
     ContributionCountOfUserIntoRepo,
-    OwnerAndRepoName,
-    SponsorableWithContributionCount,
-    Sponsorable,
+    SponsorableWithListingNameAndLink,
+    SponsorContributionHistory,
 } from './types';
 import { ContributorFetcher } from './contributorFetcher';
-
-// import { createTsv } from './createTsv';
 
 export class SponsorabilityFinder {
     private readonly config: Config;
@@ -22,7 +23,7 @@ export class SponsorabilityFinder {
     public async fetchSponsorabilityInformation(
         token: string,
         repositoryIdentifiers: string[]
-    ): Promise<Map<string, ContributionCountOfUserIntoRepo[]>> {
+    ): Promise<SponsorContributionHistory[]> {
         //SponsorWithRepoIdAndContribution
         const contributorHistoryFetcher = new ContributorFetcher(this.config);
         const sponsorableContributorsFetcher = new SponsorableContributorsFetcher(this.config);
@@ -32,21 +33,18 @@ export class SponsorabilityFinder {
             repositoryIdentifiers
         );
 
-        // const contributionCount = allContributorHistorys.values();
-        // const allContributors = [...contributionCount].flat();
+        const contributionCount = allContributorHistorys.values();
+        const allContributors = [...contributionCount].flat();
 
         const sponsorables =
             await sponsorableContributorsFetcher.fetchSponsorableContributorsInformation(
                 token,
                 queryTemplate,
-                allContributorHistorys // keep getting duplicates
+                allContributors // keep getting duplicates
             );
 
-        const sponsorableContributorWithContributonCounts =
-            this.extractContributionCountsOfAllSponsorableUsers(
-                sponsorables,
-                allContributorHistorys
-            );
+        const sponsorablesWithListingAndLink =
+            this.convertToSponsorablesWithListingAndLink(sponsorables);
 
         const repositoryFetcher = new RepoLanguagesFetcher(this.config);
         const repositoryLanguages = await repositoryFetcher.fetchAllRepositoryLanguages(
@@ -54,97 +52,100 @@ export class SponsorabilityFinder {
             repositoryIdentifiers
         );
 
-        const countsForAllLanguages = this.contributionCountsByReponame(
-            repositoryLanguages,
-            sponsorableContributorWithContributonCounts
-        );
+        // const contributorContributionCountsByRepoIdentifier =
+        //     this.extractContributionCountsOfAllSponsorableUsers(
+        //         sponsorables,
+        //         allContributorHistorys
+        //     );
 
-        console.log(countsForAllLanguages, 'language count');
+        // const countsForAllLanguages = this.contributionCountsByReponame(
+        //     repositoryLanguages,
+        //     contributorContributionCountsByRepoIdentifier
+        // );
 
-        return allContributorHistorys;
+        console.log(repositoryLanguages, 'language count');
+        console.log(sponsorablesWithListingAndLink.length);
+
+        // need to convert everything and return SponsorContributionHistory[]
+        return [];
     }
 
-    public extractContributionCountsOfAllSponsorableUsers(
-        sponsorables: Map<string, Sponsorable[]>,
-        allContributorHistorys: Map<string, ContributionCountOfUserIntoRepo[]>
-    ): Map<string, SponsorableWithContributionCount[]> {
-        const withContributionCounts = new Map<
-            OwnerAndRepoName,
-            SponsorableWithContributionCount[]
-        >();
-
-        let withCountsAndRepoID: {
-            repoId: string;
-            contributor: SponsorableWithContributionCount;
-        }[];
-
-        for (const [key, contributionCountOfUser] of allContributorHistorys) {
-            withCountsAndRepoID = this.countSponsorableContributionsByRepo(
-                sponsorables,
-                contributionCountOfUser
-            );
-
-            const storeSponsorableContributors: SponsorableWithContributionCount[] = [];
-            withCountsAndRepoID.forEach((objectWithContributorData) => {
-                if (key === objectWithContributorData.repoId) {
-                    storeSponsorableContributors.push(...[objectWithContributorData.contributor]);
-                    withContributionCounts.set(
-                        objectWithContributorData.repoId,
-                        storeSponsorableContributors
-                    );
-                }
-            });
-        }
-
-        return withContributionCounts;
+    public convertToSponsorablesWithListingAndLink(
+        sponsorables: GithubSponsorable[]
+    ): SponsorableWithListingNameAndLink[] {
+        return sponsorables.map((sponsorable) => {
+            return {
+                type: sponsorable.__typename,
+                email: sponsorable.email ?? '',
+                login: sponsorable.login,
+                url: `https://github.com/${sponsorable.login}`,
+                sponsorListingName: sponsorable.sponsorsListing.name ?? '',
+                sponsorsLink: sponsorable.sponsorsListing.dashboard ?? '',
+            };
+        });
     }
+
+    // public extractContributionCountsOfAllSponsorableUsers(
+    //     sponsorables: Sponsorable[],
+    //     allContributorHistorys: ContributorContributionCountsByRepoIdentifier
+    // ): Map<string, SponsorableWithListingNameAndLink[]> {
+    //     const withContributionCounts = new Map<string, SponsorableWithListingNameAndLink[]>();
+
+    //     let withCountsAndRepoID: {
+    //         repoId: string;
+    //         contributor: SponsorableWithListingNameAndLink;
+    //     }[];
+
+    //     for (const [key, contributionCountOfUser] of allContributorHistorys) {
+    //         withCountsAndRepoID = this.countSponsorableContributionsByRepo(
+    //             sponsorables,
+    //             contributionCountOfUser
+    //         );
+
+    //         const storeSponsorableContributors: SponsorableWithListingNameAndLink[] = [];
+    //         withCountsAndRepoID.forEach((objectWithContributorData) => {
+    //             if (key === objectWithContributorData.repoId) {
+    //                 storeSponsorableContributors.push(...[objectWithContributorData.contributor]);
+    //                 withContributionCounts.set(
+    //                     objectWithContributorData.repoId,
+    //                     storeSponsorableContributors
+    //                 );
+    //             }
+    //         });
+    //     }
+
+    //     return withContributionCounts;
+    // }
 
     /// flagging it to discuss during pairing session
-    public countSponsorableContributionsByRepo(
-        sponsorablesByReponame: Map<string, Sponsorable[]>,
-        contributionCounts: ContributionCountOfUserIntoRepo[]
-    ): {
-        repoId: string;
-        contributor: SponsorableWithContributionCount;
-    }[] {
-        const allSponsorableWithCount: {
-            repoId: string;
-            contributor: SponsorableWithContributionCount;
-        }[] = [];
+    // public countSponsorableContributionsByRepo(
+    //     sponsorablesByReponame: Map<string, Sponsorable[]>,
+    //     contributionCounts: ContributionCountOfUserIntoRepo[]
+    // ): {
+    //     repoId: string;
+    //     contributor: SponsorableWithListingNameAndLink;
+    // }[] {
+    //     const allSponsorableWithCount: {
+    //         repoId: string;
+    //         contributor: SponsorableWithListingNameAndLink;
+    //     }[] = [];
 
-        for (const [repoId, sponsorables] of sponsorablesByReponame) {
-            sponsorables.forEach((sponsorable) => {
-                const contributionsCount = this.getContributionCountOfUser(
-                    sponsorable.login,
-                    contributionCounts
-                );
+    //     for (const [repoId, sponsorables] of sponsorablesByReponame) {
+    //         sponsorables.forEach((sponsorable) => {
+    //             const contributionsCount = this.getContributionCountOfUser(
+    //                 sponsorable.login,
+    //                 contributionCounts
+    //             );
 
-                const withContributionCount: SponsorableWithContributionCount =
-                    this.convertToSponsorableWithCounts(sponsorable, contributionsCount);
+    //             const withContributionCount: SponsorableWithListingNameAndLink =
+    //                 this.convertToSponsorListingAndLink(sponsorable, contributionsCount);
 
-                allSponsorableWithCount.push({ repoId, contributor: withContributionCount });
-            });
-        }
+    //             allSponsorableWithCount.push({ repoId, contributor: withContributionCount });
+    //         });
+    //     }
 
-        return allSponsorableWithCount;
-    }
-
-    public convertToSponsorableWithCounts(
-        sponsorable: Sponsorable,
-        contributionsCount: number
-    ): SponsorableWithContributionCount {
-        const withContributionCount: SponsorableWithContributionCount = {
-            type: sponsorable.__typename,
-            email: sponsorable.email ?? '',
-            login: sponsorable.login,
-            url: `https://github.com/${sponsorable.login}`,
-            sponsorListingName: sponsorable.sponsorsListing.name ?? '',
-            sponsorsLink: sponsorable.sponsorsListing.dashboard ?? '',
-            contributionsCount: contributionsCount ?? 0,
-        };
-
-        return withContributionCount;
-    }
+    //     return allSponsorableWithCount;
+    // }
 
     public getContributionCountOfUser(
         userLogin: string,
@@ -167,83 +168,83 @@ export class SponsorabilityFinder {
     // incrementing language by only one per contributor
     */
     // countLanguagesForEachContribution;
-    public contributionCountsByReponame(
-        repoLanguages: Map<string, Languages[]>,
-        sponsorableWithContributions: Map<string, SponsorableWithContributionCount[]>
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    ): any {
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        const languageCountByRepo: any = {};
-        for (const [key, sponsorWithCounts] of sponsorableWithContributions) {
-            if (repoLanguages.has(key)) {
-                sponsorWithCounts.forEach((userContribution) => {
-                    if (userContribution.contributionsCount > 0) {
-                        if (!(key in languageCountByRepo)) {
-                            languageCountByRepo[key] = 0;
-                        }
-                        languageCountByRepo[key] += 1;
-                    } // output:  { 'pypa/pipenv': 5, 'indeedeng/util': 1 }
-                });
-            }
-        }
+    // public contributionCountsByReponame(
+    //     repoLanguages: Map<string, Languages[]>,
+    //     sponsorableWithContributions: Map<string, SponsorableWithListingAndLink[]>
+    //     // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    // ): any {
+    //     // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    //     const languageCountByRepo: any = {};
+    //     for (const [key, sponsorWithCounts] of sponsorableWithContributions) {
+    //         if (repoLanguages.has(key)) {
+    //             sponsorWithCounts.forEach((userContribution) => {
+    //                 if (userContribution.contributionsCount > 0) {
+    //                     if (!(key in languageCountByRepo)) {
+    //                         languageCountByRepo[key] = 0;
+    //                     }
+    //                     languageCountByRepo[key] += 1;
+    //                 } // output:  { 'pypa/pipenv': 5, 'indeedeng/util': 1 }
+    //             });
+    //         }
+    //     }
 
-        const languageCounts = this.languagesCount(repoLanguages, languageCountByRepo);
+    //     const languageCounts = this.languagesCount(repoLanguages, languageCountByRepo);
 
-        return languageCounts;
-    }
+    //     return languageCounts;
+    // }
 
-    public languagesCount(
-        repoLanguages: Map<string, Languages[]>,
-        languageCountByRepo: object
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    ): any {
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        const languageCountarrs: any = [];
+    // public languagesCount(
+    //     repoLanguages: Map<string, Languages[]>,
+    //     languageCountByRepo: object
+    //     // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    // ): any {
+    //     // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    //     const languageCountarrs: any = [];
 
-        const newArray = Object.entries(languageCountByRepo);
-        const countMap = new Map(newArray);
+    //     const newArray = Object.entries(languageCountByRepo);
+    //     const countMap = new Map(newArray);
 
-        repoLanguages.forEach((languages, languagesRepoID) => {
-            for (const language of languages) {
-                countMap.forEach((contributionCount, idx) => {
-                    if (languagesRepoID === idx) {
-                        // console.log(language.name, languagesRepoID);
-                        if (!(idx in languageCountarrs)) {
-                            languageCountarrs[language.name] = contributionCount;
-                        }
-                    }
-                });
-            }
-        });
-        console.log(languageCountarrs);
-        // WIP need to debug why it returns one array instead of two, per key
-        // output = [
-        //   Python: 5,
-        //   Roff: 5,
-        //   Shell: 5,
-        //   Batchfile: 5,
-        //   Makefile: 1,
-        //   Java: 1,
-        //   C: 1,
-        //   FreeMarker: 1
-        // ]
+    //     repoLanguages.forEach((languages, languagesRepoID) => {
+    //         for (const language of languages) {
+    //             countMap.forEach((contributionCount, idx) => {
+    //                 if (languagesRepoID === idx) {
+    //                     // console.log(language.name, languagesRepoID);
+    //                     if (!(idx in languageCountarrs)) {
+    //                         languageCountarrs[language.name] = contributionCount;
+    //                     }
+    //                 }
+    //             });
+    //         }
+    //     });
+    //     console.log(languageCountarrs);
+    //     // WIP need to debug why it returns one array instead of two, per key
+    //     // output = [
+    //     //   Python: 5,
+    //     //   Roff: 5,
+    //     //   Shell: 5,
+    //     //   Batchfile: 5,
+    //     //   Makefile: 1,
+    //     //   Java: 1,
+    //     //   C: 1,
+    //     //   FreeMarker: 1
+    //     // ]
 
-        // expected:
-        // array 1 =>
-        //[
-        //   Python: 5,
-        //   Roff: 5,
-        //   Shell: 5,
-        //   Batchfile: 5,
-        //]
-        // array 2 =>
-        //[
-        //   Makefile: 1,
-        //   Java: 1,
-        //   C: 1,
-        //   FreeMarker: 1
-        // ]
-    }
+    //     // expected:
+    //     // array 1 =>
+    //     //[
+    //     //   Python: 5,
+    //     //   Roff: 5,
+    //     //   Shell: 5,
+    //     //   Batchfile: 5,
+    //     //]
+    //     // array 2 =>
+    //     //[
+    //     //   Makefile: 1,
+    //     //   Java: 1,
+    //     //   C: 1,
+    //     //   FreeMarker: 1
+    //     // ]
+    // }
 }
 
 // public countLanguages(repo: string[]): string[] {
