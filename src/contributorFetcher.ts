a41eeaeb968bf9b226cd84869fc83bedb8220191
IngridGdesigns <garciai@protonmail.com>
2023-04-24 12:02:40 -0700

    edited return type

--- src/contributorFetcher.ts
+++ src/contributorFetcher.ts
@@ -1,12 +1,17 @@
-import { GitHubContributorFetcher } from './gitHubContributorFetcher';
+import { GitHubContributor, GitHubContributorFetcher } from './gitHubContributorFetcher';
 
 export class ContributorFetcher {
-    public async fetchContributors(token: string, repositoryIdentifiers: string[]): Promise<any> {
+    public async fetchContributors(
+        token: string,
+        repositoryIdentifiers: string[]
+    ): Promise<Map<string, GitHubContributor[]>> {
         const gitHubContributors = new GitHubContributorFetcher();
 
         const allGitHubContributors = await gitHubContributors.fetchContributors(
             token,
             repositoryIdentifiers
         );
+
+        return allGitHubContributors;
     }
 }
