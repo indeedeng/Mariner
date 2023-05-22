451e75ea743c4d44464d70099c417ecbeae0764b
IngridGdesigns <garciai@protonmail.com>
2023-05-22 10:41:40 -0700

    trying out spyOn in jest

--- src/__tests__/gitHubContributorFetcher.test.ts
+++ src/__tests__/gitHubContributorFetcher.test.ts
@@ -1,28 +1,95 @@
-// import { Octokit } from '@octokit/rest';
-import { GitHubContributorFetcher } from '../gitHubContributorFetcher'; // GitHubContributor
+// jest.mock('@octokit/rest');
+import { GitHubContributorFetcher, Contributor } from '../gitHubContributorFetcher'; // GitHubContributor
 
 describe('contributor fetcher class', () => {
-    const someToken = 'token';
+    const someToken = 'fakeToken';
     const contributorsFinder = new GitHubContributorFetcher(someToken);
 
-    it('should extract owner and repo names', () => {
+    it('should extract owner and repo names', async () => {
         // add more?
         const data = 'someOwner/someRepoName';
         const expectedOutput1 = { owner: 'someOwner', repo: 'someRepoName' };
 
         const extracted = contributorsFinder.extractOwnerAndRepoNames(data);
 
-        expect(expectedOutput1).toEqual(extracted);
+        return expect(expectedOutput1).toEqual(extracted);
+    });
+
+    it('it fetches Contributors', async () => {
+        const fakeContributor1: Contributor[] = [
+            {
+                login: 'awesomeContributor',
+            },
+        ];
+
+        const contributorMap = new Map<string, Contributor[]>();
+        contributorMap.set('fakeRepo1', fakeContributor1);
+
+        const repo1 = 'fakeRepo1/someCoolProject';
+        const fakeRepositoryIdentifiers = [repo1];
+
+        const getList = jest
+            .spyOn(contributorsFinder, 'fetchGitHubContributorsByRepoName')
+            .mockResolvedValue(Promise.resolve(contributorMap));
+
+        const contributorListMock = await contributorsFinder.fetchGitHubContributorsByRepoName(
+            someToken,
+            fakeRepositoryIdentifiers
+        );
+
+        expect(getList).toHaveBeenCalled();
+        expect(getList).toBeCalledWith(someToken, fakeRepositoryIdentifiers);
+        expect(contributorListMock).toBe(contributorMap);
+
+        // 'fakeRepo1/someAwesomeProject';
+        // const repo2 = 'fakeRepo1/someCoolProject';
+        // const fakeRepositoryIdentifiers = [repo1, repo2];
+
+        // const mockOctokit = new Octokit({
+        //     auth: someToken,
+        // });
+
+        // jest.spyOn(contributorsFinder, 'fetchListOfGithubContributors').mockReturnValue(
+        //     fakeContributor1
+        // );
+        // const fetchList = await contributorsFinder.fetchListOfGithubContributors(someToken, {
+        //     owner: 'fakeRepo1',
+        //     repo: 'someAwesomeProject',
+        // });
+
+        // expect(fetchList).toBe('123');
+        // expect(contributorsFinder.fetchGitHubContributorsByRepoName).toHaveBeenCalled();
+
+        // const m = mockOctokit as jest.Mocked<typeof mockOctokit>;
+
+        // // contributorsFinder.fetchGitHubContributorsByRepoName(someToken, fakeRepositoryIdentifiers);
+        // expect(Octokit).toHaveBeenCalled();
     });
 
     // it('should get a list of GitHub contributors', () => {
     //     jest.mock('@octokit/rest');
-    //     const token = 'fakeToken';
 
+    //     const mockOctokit = new Octokit({
+    //         auth: someToken,
+    //     });
+
+    // If necessary, you can place a mock implementation like this:
+    // mockOctokit.repos.listContributors()
+
+    // describe('myModule', () => {
+    //     it('calls the dependency once with double the input', () => {
+    //         myModule(2);
+
+    //         expect(dependency).toHaveBeenCalledTimes(1);
+    //         expect(dependency).toHaveBeenCalledWith(4);
+    //     });
+    // });
+
+    //     jest.mock('@octokit/rest');
+    //     const token = 'fakeToken';
     //     const mockOctokit = new Octokit({
     //         auth: token,
     //     });
-
     //     const fakeContributor1: GitHubContributor[] = [
     //         {
     //             login: 'someContributor',
@@ -73,18 +140,14 @@ describe('contributor fetcher class', () => {
     //             contributions: 7,
     //         },
     //     ];
-
     //     const repo1 = 'fakeRepo1/someAwesomeProject';
     //     const repo2 = 'fakeRepo1/someCoolProject';
     //     const fakeRepositoryIdentifiers = [repo1, repo2];
-
     //     const fakeOwnerAndRepos = [
     //         { owner: 'fakeRepo1', repo: 'someAwesomeProject' },
     //         { owner: 'fakeRepo2', repo: 'someCoolProject' },
     //     ];
-
     //     const a = mockOctokit.repos.listCollaborators(fakeOwnerAndRepos[0]);
     //     const b = mockOctokit.repos.listCollaborators(fakeOwnerAndRepos[1]);
-
     // });
 });
