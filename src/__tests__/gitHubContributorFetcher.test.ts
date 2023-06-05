bb3920aca8402e844f64f2dfda065f66d086aa29
IngridGdesigns <garciai@protonmail.com>
2023-06-05 16:21:55 -0700

    rename and pulled out fakeGitHubContributor

--- src/__tests__/gitHubContributorFetcher.test.ts
+++ src/__tests__/gitHubContributorFetcher.test.ts
@@ -9,24 +9,48 @@ describe('contributor fetcher class', () => {
     const someToken = 'fakeToken';
     const contributorsFetcher = new GitHubContributorFetcher(someToken);
 
-    it('should extract owner and repo names', async () => {
+    const fakeGitHubContributor: GitHubContributor[] = [
+        {
+            login: 'someContributor',
+            id: 6993258,
+            node_id: 'MDQ6VXNlcjY5OTMyN23=',
+            avatar_url: 'https://avatars.githubusercontent.com/u/6993258?v=4',
+            gravatar_id: '',
+            url: 'https://api.github.com/users/someContributor',
+            html_url: 'https://github.com/someContributor',
+            followers_url: 'https://api.github.com/users/someContributor/followers',
+            following_url: 'https://api.github.com/users/someContributor/following{/other_user}',
+            gists_url: 'https://api.github.com/users/someContributor/gists{/gist_id}',
+            starred_url: 'https://api.github.com/users/someContributor/starred{/owner}{/repo}',
+            subscriptions_url: 'https://api.github.com/users/someContributor/subscriptions',
+            organizations_url: 'https://api.github.com/users/someContributor/orgs',
+            repos_url: 'https://api.github.com/users/someContributor/repos',
+            events_url: 'https://api.github.com/users/someContributor/events{/privacy}',
+            received_events_url: 'https://api.github.com/users/someContributor/received_events',
+            type: 'User',
+            site_admin: false,
+            contributions: 4,
+        },
+    ];
+
+    it('extractOwnerAndRepoName', async () => {
         const data = 'someOwner/someRepoName';
         const expectedOutput1 = { owner: 'someOwner', repo: 'someRepoName' };
 
-        const extracted = contributorsFetcher.extractOwnerAndRepoName(data);
+        const extracted1 = contributorsFetcher.extractOwnerAndRepoName(data);
 
-        expect(expectedOutput1).toEqual(extracted);
+        expect(expectedOutput1).toEqual(extracted1);
     });
 
     it('fetches Contributors', async () => {
-        const fakeContributor: Contributor[] = [
+        const fakeContributorLogin: Contributor[] = [
             {
                 login: 'awesomeContributor',
             },
         ];
 
         const contributorMap = new Map<string, Contributor[]>();
-        contributorMap.set('fakeRepo1', fakeContributor);
+        contributorMap.set('fakeRepo', fakeContributorLogin);
 
         const repo = 'fakeRepo/someCoolProject';
         const fakeRepositoryIdentifiers = [repo];
@@ -46,42 +70,17 @@ describe('contributor fetcher class', () => {
     });
 
     it('fetches a GitHubContributor', async () => {
-        const fakeContributor: GitHubContributor[] = [
-            {
-                login: 'someContributor',
-                id: 6993258,
-                node_id: 'MDQ6VXNlcjY5OTMyN23=',
-                avatar_url: 'https://avatars.githubusercontent.com/u/6993258?v=4',
-                gravatar_id: '',
-                url: 'https://api.github.com/users/someContributor',
-                html_url: 'https://github.com/someContributor',
-                followers_url: 'https://api.github.com/users/someContributor/followers',
-                following_url:
-                    'https://api.github.com/users/someContributor/following{/other_user}',
-                gists_url: 'https://api.github.com/users/someContributor/gists{/gist_id}',
-                starred_url: 'https://api.github.com/users/someContributor/starred{/owner}{/repo}',
-                subscriptions_url: 'https://api.github.com/users/someContributor/subscriptions',
-                organizations_url: 'https://api.github.com/users/someContributor/orgs',
-                repos_url: 'https://api.github.com/users/someContributor/repos',
-                events_url: 'https://api.github.com/users/someContributor/events{/privacy}',
-                received_events_url: 'https://api.github.com/users/someContributor/received_events',
-                type: 'User',
-                site_admin: false,
-                contributions: 4,
-            },
-        ];
-
-        const fakeOwner = { owner: 'fakeRepo1', repo: 'someAwesomeProject' };
+        const fakeRepo = { owner: 'fakeRepo1', repo: 'someAwesomeProject' };
         const scope = nock('https://api.github.com')
-            .get(`/repos/${fakeOwner.owner}/${fakeOwner.repo}/contributors`)
-            .reply(200, fakeContributor);
+            .get(`/repos/${fakeRepo.owner}/${fakeRepo.repo}/contributors`)
+            .reply(200, fakeGitHubContributor);
 
         const gitHubContributor = await contributorsFetcher.fetchListOfGithubContributors(
             someToken,
-            fakeOwner
+            fakeRepo
         );
 
-        expect(gitHubContributor).toEqual(fakeContributor);
+        expect(gitHubContributor).toEqual(fakeGitHubContributor);
         expect(scope.isDone()).toBe(true);
         nock.cleanAll();
     });
